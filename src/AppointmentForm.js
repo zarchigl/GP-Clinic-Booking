63cb25eb9bc802898a5b2c79e3622c9ba8fe21fd
Zar <khinzarchi.nwet09@gmail.com>
2025-09-19 23:01:00 +1200

    updated api gateway

--- src/AppointmentForm.js
+++ src/AppointmentForm.js
@@ -3,7 +3,7 @@ import "./AppointmentForm.css";
 
 function buildApiUrl() {
   const DEFAULT =
-    "https://llbi3z0e1e.execute-api.us-east-1.amazonaws.com/prod/book-appointment";
+    "https://mesemhk5nh.execute-api.us-east-1.amazonaws.com/prod/book-appointment";
 
   const base = process.env.REACT_APP_APPT_API;
   if (!base) return DEFAULT;
